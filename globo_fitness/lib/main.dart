682431f153dc686ed85ba6fb0f4fcb2b3eb89fb2
altair961 <altair89@list.ru>
2024-01-06 15:55:22 +0300

    Scaffold was defined. Text and image were added.

--- globo_fitness/lib/main.dart
+++ globo_fitness/lib/main.dart
@@ -12,8 +12,35 @@ class GlobeApp extends StatelessWidget {
     return MaterialApp(
         home: Scaffold(
             appBar: AppBar(title: Text('Globo Fitness')),
-            body: Center(
-                child: Text(
-                    'Commit to be fit, dare to be great with Globo Fitness'))));
+            body: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/beach.jpg'),
+                  fit: BoxFit.cover
+                )
+              ),
+              child: Center(
+                  child: Container(
+                    padding: EdgeInsets.all(24),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white70
+                    ),
+                    child: Text(
+                        'Commit to be fit, dare to be \ngreat with Globo Fitness',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 22,
+                          shadows: [
+                            Shadow(
+                              offset: Offset(1.0, 1.0),
+                              blurRadius: 2.0,
+                              color: Colors.grey
+                            )
+                          ]
+                        )
+                        ),
+                  )),
+            )));
   }
 }
