9119d3ec27945e19f8e4eb149b2a66ec63099c65
altair961 <altair89@list.ru>
2024-06-08 22:24:19 +0300

    Presentation screen was created

--- globo_fitness/lib/main.dart
+++ globo_fitness/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(GlobeApp());
@@ -10,7 +11,28 @@ class GlobeApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(child: Text('Hello World!'))
-    );
+        home: Scaffold(
+      appBar: AppBar(title: Text('Globo Fitness')),
+      body: Container(
+        decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/beach.jpg'), fit: BoxFit.cover)),
+        child: Center(
+            child: Container(
+                padding: EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    color: Colors.white70),
+                child: Text(
+                    'Commit to be fit, dare to be great \nwith Globo Fitness',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontSize: 22, shadows: [
+                      Shadow(
+                          offset: Offset(1.0, 1.0),
+                          blurRadius: 2.0,
+                          color: Colors.grey)
+                    ])))),
+      ),
+    ));
   }
 }
