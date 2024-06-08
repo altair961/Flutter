0d51d93351c50994e2089ff403f148c4eb83b70f
altair961 <altair89@list.ru>
2024-06-08 18:00:44 +0300

    Basic widget was set up to show a center widget with a text widget saying "Hello World!"

---
+++ globo_fitness/lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(GlobeApp());
+}
+
+class GlobeApp extends StatelessWidget {
+  const GlobeApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(child: Text('Hello World!'))
+    );
+  }
+}
